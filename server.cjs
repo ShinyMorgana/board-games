0383fb735fd6d72379f04542a8075483f9c1d569
ShinyMorgana <89910002+ShinyMorgana@users.noreply.github.com>
2024-04-11 15:15:36 +0200

    s

--- server.cjs
+++ server.cjs
@@ -1,9 +1,9 @@
-import express from "express";
-import path from "path";
-import serveStatic from "serve-static";
+const express = require("express");
+const path = require("path");
+const serveStatic = require("serve-static");
 
 const app = express();
-app.use(serveStatic(path.join(path.resolve(), "dist")));
+app.use(serveStatic(path.join(__dirname, "dist")));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
