2f93db987ab61d3043c86c2ca2ee3773fed3eb62
ShinyMorgana <89910002+ShinyMorgana@users.noreply.github.com>
2024-04-13 23:32:46 +0200

    routing fix?

--- server.cjs
+++ server.cjs
@@ -1,11 +1,18 @@
-const express = require("express");
-const path = require("path");
-const serveStatic = require("serve-static");
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
 
 const app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Serve the static files from the dist directory (built Vue app)
+app.use(serveStatic(path.join(__dirname, 'dist')));
+
+// Catch all routes and lead to the index.html file
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
 });
