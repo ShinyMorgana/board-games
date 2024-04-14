eafd80e96b560d421c44f6802fde45a163901d3f
ShinyMorgana <89910002+ShinyMorgana@users.noreply.github.com>
2024-04-14 19:25:53 +0200

    zz

--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
-//axios.defaults.headers.common['X-CSRF-TOKEN'] = this.csrfToken; // geen idee alkwijhoqinfoq
 
+// Get CSRF token from meta tag
+const csrfToken = document.head.querySelector('meta[name="csrf-token"]').content;
 
 const apiClient = axios.create({
   baseURL: 'https://board-games-api-school-7a1eeb2c4b6f.herokuapp.com/api',
-  withCredentials: false,
+  withCredentials: true, // This should be true to send cookies and headers
   headers: {
-    Accept: 'application/json',
+    'Accept': 'application/json',
     'Content-Type': 'application/json',
+    'X-CSRF-TOKEN': csrfToken, // Set the CSRF token here for the client
   },
 });
 
